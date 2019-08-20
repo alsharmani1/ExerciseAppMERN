f990d364ee18177099cca50fe440cd5c06c68dac
alsharmani1 <alsharmani1@gmail.com>
2019-08-19 20:00:53 -0400

    client side

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/Navbar"; 
+import CreateExercise from "./components/CreateExercise"; 
+import CreateUser from "./components/CreateUser"; 
+import EidtExercise from "./components/EidtExercise"; 
+import ExerciseList from "./components/ExerciseList"; 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ ExerciseList } />
+      <Route path="/" exact component={ CreateExercise } />
+      <Route path="/" exact component={ CreateUser } />
+      <Route path="/" exact component={ EidtExercise } />
+    </Router>
   );
 }
 
